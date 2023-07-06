1963457e6d8b9d9be049d38d64e97fd1eed09055
damtrix <oludamola.onarinde@gmail.com>
2023-07-06 14:34:21 +0100

    9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 9's'''
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence], int]:
+    '''Annotations of a function '''
+    return [(i, len(i)) for i in lst]
