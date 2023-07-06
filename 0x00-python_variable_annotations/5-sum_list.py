a5ef5a91b7a2ababdb8d87493117a3c5a583be63
damtrix <oludamola.onarinde@gmail.com>
2023-07-06 13:45:54 +0100

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 5's'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the float sum of a floating-point list of numbers'''
+    return float(sum(input_list))
