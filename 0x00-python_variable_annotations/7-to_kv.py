0171abe0b9dceb49b516b33f1f8a11e8369e5ebf
damtrix <oludamola.onarinde@gmail.com>
2023-07-06 14:18:26 +0100

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Task's 7'''
+'''Task 7's'''
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[int, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Converts a key and its value to a tuple of the key and
     the square of its value.
     '''
