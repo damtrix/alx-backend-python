8f082321ffe2b4531d3d927a09b7cb60185276eb
damtrix <oludamola.onarinde@gmail.com>
2023-07-10 15:18:14 +0100

    task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Task's 3 module'''
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.tasks:
+    '''Return asyncio task'''
+    return asyncio.create_task(wait_random(max_delay))
