db2fedc46cd2002f8ac78b7ad1da5e70210a34fc
damtrix <oludamola.onarinde@gmail.com>
2023-07-11 15:16:14 +0100

    1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Returns a genrated list of random numbers'''
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    '''list of 10 random numbers from async_generator'''
+    return [ran async for ran in async_generator()]
---
+++ 0x02-python_async_comprehension/text.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import Generator
+
+def generate() -> Generator[int, None, None]:
+    for i in range(10):
+        yield i
+        
+l = [i for i in generate()]
+print(l)
